e8c94582053ad47f9cc531ee3f69d6336bd246bb
xnnxnyx <annanya21sharma@gmail.com>
2023-11-07 20:36:40 -0500

    chore: adding data

--- backend/connection.mjs
+++ backend/connection.mjs
@@ -12,8 +12,18 @@ const connectToServer = async (callback) => {
     try {
       await client.connect();
       console.log('Successfully connected to MongoDB ... ');
-      db = client.db('admin');
-      //const collection = db.collection('some_collection in the admin db');
+      // creates a new db called testing 
+      db = client.db('testing');
+      // creates a collection in testing
+      const coll = db.collection("comets");
+      const docs = [
+        {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
+        {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
+        {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
+      ];
+      const result = await coll.insertMany(docs);
+      // display the results of your operation
+      console.log(result.insertedIds);
     } catch (err) {
     console.error(err.message);
   }
