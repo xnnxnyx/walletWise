05c55ae11d59268e6c3b7092cbaa3d4cd6878398
Jumana <jumana.fanous@mail.utoronto.ca>
2023-11-21 17:40:58 -0500

    more login changes

--- frontend/app/pages/login.jsx
+++ frontend/app/pages/login.jsx
@@ -11,6 +11,9 @@ function LoginPage() {
         <div className='page'>
             <div className='center'>
                 <div className='login'>
+                <h1 className='logo'>
+                    WalletWise
+                </h1>
                     <div className='pic'>
                         <div className='uploadpic'>
                         </div>
@@ -21,12 +24,15 @@ function LoginPage() {
                     <div className='input'>
                         <h1 className='content'>Email:</h1>
                         <div className='email'>
+                        <input type="email" className='email' placeholder="first.last@mail.com" />
                         </div>
                         <h1 className='content'>First and Last Name:</h1>
                         <div className='username'>
+                        <input type="username" className='username' placeholder="first and last name" />
                         </div>
                         <h1 className='content'>Password:</h1>
                         <div className='password'>
+                        <input type="password" className='password' placeholder="at least 6 chars and 1 special char" />
                         </div>
                     </div>
                     <div className='click'>
