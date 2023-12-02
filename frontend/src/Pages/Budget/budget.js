11f2ac2c4108b1b16df0e4346b5301a71ae1b751
Jumana <jumana.fanous@mail.utoronto.ca>
2023-12-01 19:28:47 -0500

    budgets styling changes

--- frontend/src/Pages/Budget/budget.js
+++ frontend/src/Pages/Budget/budget.js
@@ -101,14 +101,14 @@ export const BudgetPage = () =>{
     return (
     <div className="screen">
       <div className="page">
-        <div className="center">
+        <div className="cen">
             <Sidebar/>
             {/* <div className='title'>
                 <h1>Expenses</h1>
                 <p> welcome  to your financial command center for tracking and managing all your spending effortlessly</p>
             </div> */}
-            <div className='middle'>
-            <div className='four'>
+            <div className='m'>
+            <div className='f'>
             {Object.entries(categoryMap).map(([category, [spent, remaining]], index) => (
                 <Pie key={index} category={category} spent={spent} remaining={remaining} />
             ))}
--- frontend/src/partials/PieChart/pie.js
+++ frontend/src/partials/PieChart/pie.js
@@ -3,42 +3,45 @@ import { PieChart } from '@mui/x-charts/PieChart';
 import { Typography, Stack } from '@mui/material';
 import Card from '../../partials/Cards/cards';
 
-export default function Pie( { category, spent, remaining } ) {
-  const [identifier, setIdentifier] = React.useState(null);
-  const [id, setId] = React.useState(undefined);
-
-
-
-  // Generate items array from category map
+export default function Pie({ category, spent, remaining }) {
   const items = [
-    { value: spent, label: "Spent" },
-    { value: remaining, label: "Remaning" },
+    { value: spent, label: 'Spent', color: '#BFB8DA' },
+    { value: remaining, label: 'Remaining', color: '#F3DBCF' },
   ];
 
   return (
     <Card>
-    <Stack
-      direction={{ xs: 'column', md: 'column' }}
-      alignItems={{ xs: 'center', md: 'center' }}
-      justifyContent="space-between"
-      sx={{ width: '100%' }}
-    >
+      <Stack
+        direction={{ xs: 'column', md: 'column' }}
+        alignItems={{ xs: 'center', md: 'center' }}
+        justifyContent="space-between"
+        sx={{ width: '100%', marginTop: '20px' }}
+      >
+        <Typography
+          style={{
+            color: '#0D0447',
+            fontFamily: 'Arial, Helvetica, sans-serif',
+            fontSize: '25px',
+            fontStyle: 'normal',
+            fontWeight: '550',
+          }}
+        >
+          {category}
+        </Typography>
 
-    <Typography style={{ color: 'blue', fontSize: '16px', textAlign: 'left' }}>{category}</Typography>
-
-      <PieChart
-        series={[
-          {
-            data: items,
-            highlightScope: { faded: 'global', highlighted: 'item' },
-            faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-          },
-        ]}
-        width={400}
-        height={200}
-        margin={{ right: 200 }}
-      />
-    </Stack>
+        <PieChart
+          series={[
+            {
+              data: items,
+              highlightScope: { faded: 'global', highlighted: 'item' },
+              faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+            },
+          ]}
+          width={400}
+          height={200}
+          margin={{ right: 200 }}
+        />
+      </Stack>
     </Card>
   );
 }
