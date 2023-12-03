2f5b5e83ad98874e7e8690cf1b2f18c77adfe801
xnnxnyx <annanya21sharma@gmail.com>
2023-12-03 01:50:06 -0500

    chore: updated rendering

--- frontend/src/components/QRcode/qrcode.js
+++ frontend/src/components/QRcode/qrcode.js
@@ -12,7 +12,7 @@ const QRcomp = () => {
   useEffect(() => {
     const encodedUsername = encodeURIComponent(type);
     const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSegfDHMs8kLda5nswiIH4LFUmAw396kBWxMvY5h49wQTikLzA/viewform?entry.1076970991=${encodedUsername}`;
-
+    qrCodeRef.current.innerHTML = '';
     const qrCode = new QRCodeStyling({
       data: formUrl,
       width: 180,
